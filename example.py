2f10fb317ecf77fa139f59de816bfe1fe4dbe254
BemusedButBloodthirsty <59654637+BemusedButBloodthirsty@users.noreply.github.com>
2021-06-10 17:46:52 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,30 +1,52 @@
 from section import CrossSection
 
-# Specify the points of the outer boundary of the cross-section, listed either in the clockwise or counterclockwise direction.
-points = [
+
+# Create a new cross-section:
+# ---------------------------------
+points = [ 
+    # Specify the points of the outer boundary of the cross-section, 
+    # listed either in the clockwise or counterclockwise direction.
     [0,0],
     [0,1],
     [1,1],
     [1,0]
 ]
+cs = CrossSection(points)
 
-# Create a new cross-section object:
-cs = CrossSection()
 
-# Define the geometry of the cross-section:
-cs.setBoundaryPoints(points)
-cs.addCircularHole(centre=[0.5,0.5], r=0.2, n=50)
-# cs.addRectangularHole(centre=[0.5,0.5], height=0.25, width=0.75)
-# cs.addCustomHole(inside_point=[0.5,0.5], hole_points=[[0.1,0.1],[0.5,0.9],[0.9,0.1]])
+# Add optional holes to the cross-section:
+# ---------------------------------------
+# cs.AddCircularHole(
+#   centre=[0.5,0.5], 
+#   r=0.2, 
+#   n=50
+# )
+#
+cs.AddRectangularHole(
+  centre=[0.5,0.5], 
+  height=0.25, 
+  width=0.75
+)
+# 
+# cs.AddCustomHole(
+#   inside_point=[0.5,0.5], 
+#   hole_points=[[0.1,0.1],[0.5,0.9],[0.9,0.1]]
+# )
 
-# Triangulate the section:
-cs.setElementArea(1) # NOTE: Defaults to 1 if this line is omitted.
-cs.TriangulateSection()
 
-# Plot the mesh:
-cs.PlotMesh()
+# Yout can change the element area of each triangle:
+# -------------------------------------------------
+cs.ChangeElementArea(0.01)
+
+
+# Plot the triangulated mesh:
+# --------------------------
+cs.Plot()
+
 
 # Calculate the properties:
+# ------------------------
 print("Area:", cs.Area())
 print("Centroid (X, Y):", cs.Centroid())
-print(f"Moment of Inertia (XX, YY, XY):", cs.MomentOfInertia())
+print("Moment of Inertia (XX, YY, XY):", cs.MomentOfInertia())
+# print("Moment of area: ", cs.MomentOfArea(extra_hole=[0.5,0.6]))
