43e2da3ad9649336ebec89b2c19d84a0d9bbe622
BemusedButBloodthirsty <59654637+BemusedButBloodthirsty@users.noreply.github.com>
2021-06-10 17:46:34 +0200

    Update section.py

--- section.py
+++ section.py
@@ -4,161 +4,101 @@ import triangle as tr
 
 class CrossSection:
 
-	def __init__(self):
-		self.pts = []
-		self.segs = []
-		self.hls = []
-		self.data = None
-		self.element_area = 1
-		self.triangulated = False
-		self.tris = None
-		self.section_boundary_specified = False
-		self.areas = None
-		self.x_centroids = None
-		self.y_centroids = None
-		self.x_coords = None
-		self.y_coords = None
-		self.moi_x = None
-		self.moi_y = None
+	def __init__(self, points_list):
+		self.__pts = []
+		self.__segs = []
+		self.__hls = []
+		self.__data = []
+		self.__element_area = 1
+		self.__tris = []
+		# self.__section_boundary_specified = False
+		self.__areas = []
+		self.__x_centroids = []
+		self.__y_centroids = []
+		self.__x_coords = []
+		self.__y_coords = []
+		self.__moi_x = []
+		self.__moi_y = []
+		self.__moi_xy = []
 
-	def getStatus(self):
-		print(self.triangulated)
-		print(self.section_boundary_specified)
+		self.__set_boundary_points(points_list)
+		self.__triangulate_section()
 
-	def setBoundaryPoints(self, boundary_points):
-		if len(boundary_points) > 3:
-			self.section_boundary_specified = True
-		self.pts.extend(boundary_points)
-		self.segs.extend(self.__createSegments(self.pts))
+	# def GetBoundaryPoints(self):
+	# 	return self.__pts
 
-	def getBoundaryPoints(self):
-		return self.pts
+	# def GetStatus(self):
+	# 	print(self.__triangulated)
+	# 	print(self.__section_boundary_specified)
 
-	def setElementArea(self, area: float):
-		self.element_area = area
-
-	def addCustomHole(self, inside_point, hole_points):
-		if not self.section_boundary_specified:
-			print("Cross section boundary points have not been specified yet. Use the __object__.setBoundaryPoints() method first.")
-
-		if self.triangulated:
-			print("Section has already been triangulated. First add all holes before __object__.TriangulateSection() is used.")
-
-		self.pts.extend(hole_points)
-		self.segs.extend(self.__createSegments(hole_points))
-		self.hls.append(inside_point)
-
-	def addRectangularHole(self, centre, height, width):
-		if not self.section_boundary_specified:
-			print("Cross section boundary points have not been specified yet. Use the __object__.setBoundaryPoints() method first.")
-
-		if self.triangulated:
-			print("Section has already been triangulated. First add all holes before __object__.TriangulateSection() is used.")
-
-		x, y = centre[0], centre[1]
-
-		rectangle_points = [
-			[x - width/2, y - height/2],
-			[x + width/2, y - height/2],
-			[x + width/2, y + height/2],
-			[x - width/2, y + height/2]
-		]
-
-		self.pts.extend(rectangle_points)
-		self.segs.extend(self.__createSegments(rectangle_points))
-		self.hls.append(centre)
-
-	def addCircularHole(self, centre, r, n=50):
-		"""
-		centre: list containing the x and y coordinates of the centre of the circle to be cut out of the section,
-		r: radius of the circle, and
-		n: number of points which define the circumference of the circle.
-		"""
-		if not self.section_boundary_specified:
-			print("Cross section boundary points have not been specified yet. Use the __object__.setBoundaryPoints() method first.")
-
-		if self.triangulated:
-			print("Section has already been triangulated. First add all holes before __object__.TriangulateSection() is used.")
-
-		x0, y0 = centre[0], centre[1]
-		circle_pts = [[ x0 + cos(2*pi/n*x)*r, y0 + sin(2*pi/n*x)*r ] for x in range(0,n)]
-		self.pts.extend(circle_pts)
-		self.segs.extend(self.__createSegments(circle_pts))
-		self.hls.append(centre)
-
-	def TriangulateSection(self):
-		self.triangulated = True
-		if len(self.hls) >  0:
-			self.data = dict(vertices=self.pts, segments=self.segs, holes=self.hls)
+	def __clamp(self, var, var_min, var_max):
+		if var < var_min:
+			return var_min, True
+		elif var > var_max:
+			return var_max, True
 		else:
-			self.data = dict(vertices=self.pts, segments=self.segs)
-		self.tris = tr.triangulate(self.data, opts=f"qpa{self.element_area}")
-		self.areas = zeros(len(self.tris['triangles']))
+			return var, False
 
-	def PlotMesh(self):
-		if not self.triangulated:
-			print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
-		else:
-			plt.figure()
-			plt.axis('equal')
-			plt.triplot(self.tris['vertices'][:, 0], self.tris['vertices'][:, 1], self.tris['triangles'])
-			plt.show()
 
-	def __createSegments(self, geom_pts, closed=True):
+	def __create_segments(self, geom_pts, closed=True):
 		segments = []
-		start = len(self.segs)
+		start = len(self.__segs)
 		for i in range(len(geom_pts)):
 			if i == len(geom_pts)-1:
 				if closed:
-					self.segs.append([start + i, start])
+					self.__segs.append([start + i, start])
 			else:
-				self.segs.append([start + i, start + (i+1)])
+				self.__segs.append([start + i, start + (i+1)])
 		return segments
 
-	def __create_coords_lists(self):
-		self.x_coords = []
-		self.y_coords = []
 
-		for indx, t in enumerate(self.tris['triangles']):
-			tri_points = self.tris['vertices'][t]
+	def __set_boundary_points(self, boundary_points):
+		# if len(boundary_points) > 3:
+		# 	self.__section_boundary_specified = True
+		self.__pts.extend(boundary_points)
+		self.__segs.extend(self.__create_segments(self.__pts))
+	
+
+	def __triangulate_section(self):
+		# self.__triangulated = True
+		if len(self.__hls) >  0:
+			self.__data = dict(vertices=self.__pts, segments=self.__segs, holes=self.__hls)
+		else:
+			self.__data = dict(vertices=self.__pts, segments=self.__segs)
+		self.__tris = tr.triangulate(self.__data, opts=f"qpa{self.__element_area}")
+		self.__areas = zeros(len(self.__tris['triangles']))
+	
+	
+	def __create_coords_lists(self):
+		for indx, t in enumerate(self.__tris['triangles']):
+			tri_points = self.__tris['vertices'][t]
 			x1, y1 = tri_points[0][0], tri_points[0][1]
 			x2, y2 = tri_points[1][0], tri_points[1][1]
 			x3, y3 = tri_points[2][0], tri_points[2][1]
-			self.x_coords.append([x1, x2, x3])
-			self.y_coords.append([y1, y2, y3])
+			self.__x_coords.append([x1, x2, x3])
+			self.__y_coords.append([y1, y2, y3])
+
 
 	def __calculate_tri_areas(self):
-		for indx, t in enumerate(self.tris['triangles']):
-			tri_points = self.tris['vertices'][t]
+		for indx, t in enumerate(self.__tris['triangles']):
+			tri_points = self.__tris['vertices'][t]
 			x1, y1 = tri_points[0][0], tri_points[0][1]
 			x2, y2 = tri_points[1][0], tri_points[1][1]
 			x3, y3 = tri_points[2][0], tri_points[2][1]
-			self.areas[indx] = 0.5*((x1*y2 - x2*y1) + (x3*y1 - x1*y3) + (x2*y3 - x3*y2))
+			self.__areas[indx] = 0.5*((x1*y2 - x2*y1) + (x3*y1 - x1*y3) + (x2*y3 - x3*y2))
+
 
 	def __calculate_tri_centroids(self):
-		self.x_centroids = zeros(len(self.areas))
-		self.y_centroids = zeros(len(self.areas))
+		self.__x_centroids = zeros(len(self.__areas))
+		self.__y_centroids = zeros(len(self.__areas))
 
-		for indx, t in enumerate(self.tris['triangles']):
-			tri_points = self.tris['vertices'][t]
+		for indx, t in enumerate(self.__tris['triangles']):
+			tri_points = self.__tris['vertices'][t]
 			x1, y1 = tri_points[0][0], tri_points[0][1]
 			x2, y2 = tri_points[1][0], tri_points[1][1]
 			x3, y3 = tri_points[2][0], tri_points[2][1]
-			self.x_centroids[indx] = (x1 + x2 + x3) / 3
-			self.y_centroids[indx] = (y1 + y2 + y3) / 3
-
-	def Area(self):
-		if not self.triangulated:
-			print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
-		self.__calculate_tri_areas()
-		return sum(self.areas)
-
-	def Centroid(self):
-		if not self.triangulated:
-			print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
-		self.__calculate_tri_areas()
-		self.__calculate_tri_centroids()
-		return sum(self.areas*self.x_centroids) / sum(self.areas), sum(self.areas*self.y_centroids) / sum(self.areas)
+			self.__x_centroids[indx] = (x1 + x2 + x3) / 3
+			self.__y_centroids[indx] = (y1 + y2 + y3) / 3
 
 
 	def __tri_moi(self, tri_points):
@@ -172,8 +112,8 @@ class CrossSection:
 
 		# MOI about the origin:
 		# --------------------
-		dx: float = 0
-		dy: float = 0
+		# dx: float = 0
+		# dy: float = 0
 
 		Ix_origin:  float = 0
 		Iy_origin:  float = 0
@@ -183,9 +123,9 @@ class CrossSection:
 			x_i, x_iplus1 = tri_points[i][0], tri_points[(i+1) % 3][0] # Making sure n+1 = 1.
 			y_i, y_iplus1 = tri_points[i][1], tri_points[(i+1) % 3][1]
 			
-			# Data for the centroid calculations:
-			dx += x_i
-			dy += y_i 
+			# # Data for the centroid calculations:
+			# dx += x_i
+			# dy += y_i 
 			
 			# Formula for 2x the signed area of the elementary tri: a_i = x_i * y_i+1 - x_i+1 * y_i
 			a_i = x_i * y_iplus1 - x_iplus1 * y_i # This is 2x the area of the triangle.
@@ -223,43 +163,106 @@ class CrossSection:
 		return Ix_origin, Iy_origin, Ixy_origin
 
 
+	def AddCustomHole(self, inside_point, hole_points):
+		# if not self.__section_boundary_specified:
+		# 	print("Cross section boundary points have not been specified yet. Use the __object__.setBoundaryPoints() method first.")
+
+		# if self.__triangulated:
+		# 	print("Section has already been triangulated. First add all holes before __object__.TriangulateSection() is used.")
+
+		self.__pts.extend(hole_points)
+		self.__segs.extend(self.__create_segments(hole_points))
+		self.__hls.append(inside_point)
+		self.__triangulate_section()
+
+
+	def AddRectangularHole(self, centre, height, width):
+		# if not self.__section_boundary_specified:
+		# 	print("Cross section boundary points have not been specified yet. Use the __object__.setBoundaryPoints() method first.")
+
+		# if self.__triangulated:
+		# 	print("Section has already been triangulated. First add all holes before __object__.TriangulateSection() is used.")
+
+		x, y = centre[0], centre[1]
+
+		rectangle_points = [
+			[x - width/2, y - height/2],
+			[x + width/2, y - height/2],
+			[x + width/2, y + height/2],
+			[x - width/2, y + height/2]
+		]
+
+		self.__pts.extend(rectangle_points)
+		self.__segs.extend(self.__create_segments(rectangle_points))
+		self.__hls.append(centre)
+		self.__triangulate_section()
+
+
+	def AddCircularHole(self, centre, r, n=50):
+		"""
+		centre: list containing the x and y coordinates of the centre of the circle to be cut out of the section,
+		r: radius of the circle, and
+		n: number of points which define the circumference of the circle.
+		"""
+		# if not self.__section_boundary_specified:
+		# 	print("Cross section boundary points have not been specified yet. Use the __object__.setBoundaryPoints() method first.")
+
+		# if self.__triangulated:
+		# 	print("Section has already been triangulated. First add all holes before __object__.TriangulateSection() is used.")
+
+		x0, y0 = centre[0], centre[1]
+		circle_pts = [[ x0 + cos(2*pi/n*x)*r, y0 + sin(2*pi/n*x)*r ] for x in range(0,n)]
+		self.__pts.extend(circle_pts)
+		self.__segs.extend(self.__create_segments(circle_pts))
+		self.__hls.append(centre)
+		self.__triangulate_section()
+
+
+	def ChangeElementArea(self, area: float):
+		self.__element_area = area
+		self.__triangulate_section()
+
+
+	def Plot(self, tris_only=False):
+		# if not self.__triangulated:
+		# 	print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
+		# else:
+		if tris_only:
+			plt.figure()
+			plt.axis('equal')
+			plt.triplot(self.__tris['vertices'][:, 0], self.__tris['vertices'][:, 1], self.__tris['triangles'])
+			plt.show()
+		else:
+			tr.compare(plt, self.__data, self.__tris)
+			plt.show()
+
+	
+	def Area(self):
+		# if not self.__triangulated:
+		# 	print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
+		self.__calculate_tri_areas()
+		return sum(self.__areas)
+
+
+	def Centroid(self):
+		# if not self.__triangulated:
+		# 	print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
+		self.__calculate_tri_areas()
+		self.__calculate_tri_centroids()
+		return sum(self.__areas*self.__x_centroids) / sum(self.__areas), sum(self.__areas*self.__y_centroids) / sum(self.__areas)
+
+
 	def MomentOfInertia(self):
-		if not self.triangulated:
-			print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
-
-		# self.__calculate_tri_areas()
-		# self.__calculate_tri_centroids()
-		# self.__create_coords_lists()
-
-		# section_x_centroid, section_y_centroid = self.Centroid()
-
-		# self.moi_x = zeros(len(self.areas))
-		# self.moi_y = zeros(len(self.areas))
-
-		# for indx, (x, y) in enumerate(zip(self.x_coords, self.y_coords)):
-		# 	x_max, x_min = max(x), min(x)
-		# 	y_max, y_min = max(y), min(y)
-		# 	b = abs(x_max - x_min)
-		# 	h = abs(y_max - y_min)
-		# 	x_sorted = sorted(x)
-		# 	a = abs(x_sorted[1] - x_sorted[0])
-
-		# 	self.moi_x[indx] = b*h**3/36 + self.areas[indx]*(self.y_centroids[indx] - section_y_centroid)**2
-		# 	self.moi_y[indx] = (b**3*h - b**2*h*a + b*h*a**2)/36 + self.areas[indx]*(self.x_centroids[indx] - section_x_centroid)**2
-
-		#return sum(self.moi_x), sum(self.moi_y)
+		# if not self.__triangulated:
+		# 	print("Mesh for the section has not been triangulated yet. Use the __object__.TriangulateSection() method first.")
 
 		self.__create_coords_lists()
 
-		a 	   = self.Area()
+		A 	   = self.Area()
 		dx, dy = self.Centroid()
 
-		Ix_origin:  float = 0
-		Iy_origin:  float = 0
-		Ixy_origin: float = 0
-
 		# Sum I for each triangle, about the origin:
-		for x, y in zip(self.x_coords, self.y_coords):
+		for x, y in zip(self.__x_coords, self.__y_coords):
 
 			# Reformat the list:
 			t = [
@@ -268,17 +271,72 @@ class CrossSection:
 				[x[2], y[2]]
 			]
 
-			Ix, Iy, Ixy = self.__tri_moi(t)
+			Ix_tri, Iy_tri, Ixy_tri = self.__tri_moi(t)
 
-			Ix_origin += Ix
-			Iy_origin += Iy
-			Ixy_origin += Ixy
+			self.__moi_x.append(Ix_tri)
+			self.__moi_y.append(Iy_tri)
+			self.__moi_xy.append(Ixy_tri)
 
 		# The I values about the section centroid are given as follows:
-		Ix = Ix_origin - a*dy**2
-		Iy = Iy_origin - a*dx**2
-		Ixy = Ixy_origin - a*abs(dx)*abs(dy)
+		Ix  = sum(self.__moi_x)  - A * dy**2
+		Iy  = sum(self.__moi_y)  - A * dx**2
+		Ixy = sum(self.__moi_xy) - A * abs(dx)*abs(dy)
 
 		return Ix, Iy, Ixy
 
+
+	def MomentOfArea(self, y=0, extra_hole=None):
+		"""
+		y: Distance specified above or below the neutral axis of the section. Clamped to the min and max y-coordinate specified in the boundary points.
 		
+		Defaults to the centroid if not specified (i.e. maximum moment of area).
+		"""
+		yc = self.Centroid()[1]
+		if y == 0:
+			y = yc
+		else:
+			y += yc
+
+		x_vals = [row[0] for row in self.__pts]
+		y_vals = [row[1] for row in self.__pts]
+
+		x_min, x_max = min(x_vals), max(x_vals)
+		y_min, y_max = min(y_vals), max(y_vals)
+
+		y, clamped = self.__clamp(y, y_min, y_max) # Restrict the value of the y coordinate to the min/max possible.
+
+		boundary = [[x_min, y], [x_max, y]]
+
+		old_pts = self.__pts.copy()
+		old_segs = self.__segs.copy()
+		old_hls = self.__hls.copy()
+
+		self.__pts.extend(boundary)
+		self.__segs.extend(self.__create_segments(boundary))
+
+		if extra_hole != None:
+			self.__hls.append(extra_hole)
+
+		self.__triangulate_section()
+		yc = self.Centroid()[1]
+		
+		self.Plot()
+		print("Clamped:", clamped)
+
+		Q = 0
+		if not clamped:
+			# If the value was clamped, this means that the boundary edges are selected,
+			# thus it means that the Q value will be zero.
+		
+			for tri_area, tri_y_centroid in zip(self.__areas, self.__y_centroids):
+				if tri_y_centroid > yc:
+					# This can be used to calculate the Q value:
+					Q += abs(tri_y_centroid - yc) * tri_area 
+
+		# Restore the previous triangulation:
+		self.__pts = old_pts
+		self.__segs = old_segs
+		self.__hls = old_hls
+		self.__triangulate_section()
+		# self.Plot()
+		return Q		
